acb3a20f4282cdf0a4bbfeaa8310a7d33adce26b
EshietIy <eshietinyang@gmail.com>
2021-09-22 23:46:37 +0100

    header file

---
+++ monty.h
@@ -0,0 +1,50 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+extern int sq_flag;
+
+/*--- Struct Definitions ---*/
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+
+typedef struct stack_s
+{
+int n;
+struct stack_s *prev;
+struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+
+typedef struct instruction_s
+{
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+typedef void (*instruct_func)(stack_t **stack, unsigned int line_number);
+void read_file(char *filename, stack_t **stack);
+instruct_func get_op_func(char *str);
+char *parse_line(char *line);
+void error_exit(stack_t **stack);
+void free_dlistint(stack_t *head);
+#endif
